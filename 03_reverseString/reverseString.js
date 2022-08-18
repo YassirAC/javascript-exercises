1c810500689b897d16461dbd12d9f6f1fe61a978
Yassir Anwar-Choudhury <yassirac@proton.me>
2022-08-18 17:01:48 +0100

    Task 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let splitString = text.split('')
+    let reverseString = splitString.reverse()
+    let reversedString = reverseString.join('')
+
+    return reversedString;
+
 
 };
 
+reverseString('hello');
+reverseString('123! abc!');
 // Do not edit below this line
 module.exports = reverseString;
