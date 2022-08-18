56d7808b6e9fdd2dca70c0ef706c7196af59a562
Yassir Anwar-Choudhury <yassirac@proton.me>
2022-08-18 16:20:36 +0100

    Completed 2nd task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    if (num === 0 || str === ''){
+        return ''
+    }
+    else if (num < 0){
+        return 'ERROR'
+    }
+
+    else if (num > 0){
+        return str.repeat(num)
+    }
+    else{
+        return 'undefined'
+    }
+
+    
 
 };
+const number = Math.floor(Math.random() * 1000)
+
+repeatString('hey',number);
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
