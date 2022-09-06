cd3377e261ad5fa88103fc4656e91c4863e9113b
Yassir Anwar-Choudhury <yassirac@proton.me>
2022-09-06 15:10:14 +0100

    Leap year exercise complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(x) {
+    if (x%400 === 0){
+        return true
+    }
+    else if (x%100===0 && x%4===0){
+        return false
+    }
+    else if(x%4===0){
+        return true
+
+    }
+    else{
+        return false
+    }
 
 };
 
