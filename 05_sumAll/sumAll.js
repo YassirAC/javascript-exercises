e86476fa813a17aab25c2104f8c1af322b12c5f6
Yassir Anwar-Choudhury <yassirac@proton.me>
2022-09-06 14:51:06 +0100

    Exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
 
-};
+    let sum = 0;
+
+if(!Number.isInteger(x) || !Number.isInteger(y)){
+    return 'ERROR';
+}
+
+else if(y>x && x>0){
+    for(i = x; i <= y; i++){
+            sum = sum + i;
+
+    }
+}
+else if(x>y && y>0){
+    for(i = y; i <= x; i++){
+        sum = sum + i;
+
+}
+}
+else{
+    return 'ERROR';
+}
+return sum;
+
+}
+    
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
