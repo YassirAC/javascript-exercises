cbf9d82e31103f71d24f3c565a80319c7cc03a9c
Yassir Anwar-Choudhury <yassirac@proton.me>
2022-09-06 12:38:30 +0100

    Completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    // first argument is the array which is pulled out of the argument with args[0]
+    const array = args[0];
+    // create a new empty array below
+    const newArray = [];
+    // the forEach is used to go through the array
+    array.forEach((item) => {
+     if (!args.includes(item)){
+        newArray.push(item);
+    } 
+    });
+
+    return newArray;
+
+
+
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
